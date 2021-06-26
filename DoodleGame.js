2797e9af8196b26af0cf3c482fab8436f5a81733
polus314 <johnpolus@protonmail.com>
2021-06-25 21:35:12 -0500

    Generating chunks of platforms as Doodle moves up the screen, adding breakable platforms

--- DoodleGame.js
+++ DoodleGame.js
@@ -1,8 +1,8 @@
 // Global variables
 var frames = 0;
 var doodle = {};
-var platform = {};
 var background = {};
+var platformImg = {};
 var platforms = [];
 var bump_scroll= 0;
 
@@ -14,22 +14,23 @@ const MAX_X_VEL = 8;
 const NOSE_WIDTH = 14;
 const HEAD_BUMP = 200;
 
-var chunk_library = {
-	1: "10 10 solid,20 20 solid,30 30 solid,40 40 solid,30 50 solid,20 60 solid,10 70 solid",
-	2: "10 10 solid,30 30 solid,10 50 solid,30 70 solid,10 90 solid",
-	3: "10 10 solid,20 10 solid,30 30 solid,40 30 solid,10 50 solid,20 50 solid,30 70 solid, 40 70 solid",
-	4: "10 10 solid,10 40 solid,10 70 solid,10 100 solid",
-	5: "10 10 solid,20 20 solid,30 30 solid,40 40 solid,30 50 solid,20 60 solid,10 70 solid",
-}
+var chunk_library = new Array(
+	//'70 100 solid,140 200 solid,210 300 solid,280 400 solid,210 500 solid,140 600 solid,70 700 solid',
+	//'70 100 solid,210 300 solid,70 500 solid,210 700 solid',
+	//'70 100 solid,140 100 solid,210 300 solid,280 300 solid,70 500 solid,140 500 solid,210 700 solid,280 700 solid',
+	//'70 100 solid,70 300 solid,70 500 solid,70 700 solid',
+	'70 100 solid,140 200 solid,210 300 solid,280 400 solid,210 500 solid,140 600 solid,70 700 solid',
+	'70 100 breakable,140 100 breakable,210 300 breakable,280 300 breakable,70 500 breakable,140 500 breakable,210 700 breakable,280 700 breakable',
+);
 
-function createPlatformChunk() {
-	var platformChunk = chunk_library[5];
+function createPlatformChunk(platformChunk, offset) {
+	console.log(platformChunk);
 	var platformInfo = platformChunk.split(",");
 	platformInfo.forEach( function (item, index, array) {
 		var arrInfo = item.split(" ");
 		var platform = {};
-		platform.x = arrInfo[0] * 7;
-		platform.y = SCREEN_HEIGHT - (arrInfo[1] * 7);
+		platform.x = Number(arrInfo[0]);
+		platform.y = Number(SCREEN_HEIGHT - (arrInfo[1]) + offset);
 		platform.type = arrInfo[2];
 		platform.width = 64;
 		platform.height = 32;
@@ -91,11 +92,29 @@ function verticalLogic(){
 		bump_scroll = doodle.y - HEAD_BUMP;
 		doodle.y = HEAD_BUMP;
 		background.y -= bump_scroll / 2;//pan background
-		platform.y -= bump_scroll;
 		platforms.forEach(function (item, index, array){
 			item.y -= bump_scroll;
 		})
 		
+		// Remove platforms that have scrolled off screen
+		var i = platforms.length;
+		var yMinimum = 0;
+		while (i--) {
+			var yVal = platforms[i].y;
+			if (yVal < yMinimum) {
+				yMinimum = yVal;
+			}
+			if (yVal > SCREEN_HEIGHT) {
+				platforms.splice(i, 1);
+			}
+		}
+		
+		if (yMinimum > -10) {
+			console.log(yMinimum);
+			var randomIndex = Math.floor(Math.random() * (chunk_library.length - 0.1));
+			var chunkInfo = chunk_library[randomIndex];
+			createPlatformChunk(chunkInfo, yMinimum-SCREEN_HEIGHT);
+		}
 	}
 	
 	
@@ -111,8 +130,8 @@ function updateDoodle(){
 	verticalLogic();
 	
 	//collision of platform:
-	detectCollision(platform);
 	platforms.forEach(function(item) {detectCollision(item)});
+	console.log(platforms.length);
 		
 	//edge warp doodle each side
 	if (doodle.x < -doodle.width / 2) {
@@ -122,7 +141,7 @@ function updateDoodle(){
 		doodle.x = (-doodle.width / 2) + 5;
 	}
 		//DEBUG (respawn)
-	if (doodle.y > 1000) {
+	if (doodle.y > 720) {
 		doodle.y = HEAD_BUMP;
 		doodle.x = 0;
 	}
@@ -130,13 +149,16 @@ function updateDoodle(){
 
 
 function detectCollision(myPlatform) {
-	if (myPlatform.x - doodle.width < doodle.x - NOSE_WIDTH && doodle.x + NOSE_WIDTH < myPlatform.x + myPlatform.width)
+	if ((myPlatform.x - doodle.width < doodle.x - NOSE_WIDTH) && (doodle.x + NOSE_WIDTH < myPlatform.x + myPlatform.width))
 	{
 		if (doodle.y + doodle.height - MAX_Y_VEL - 1 < myPlatform.y && myPlatform.y < doodle.y + doodle.height)
 		{
 			if (doodle.yVel > 0) 
 			{
 				doodle.yVel = -16;
+				if (myPlatform.type == "breakable") {
+					myPlatform.broken = true;
+				}
 			}
 		}
 	}
@@ -148,15 +170,25 @@ function updateBackground(){
 	}		
 }
 
+function updatePlatforms() {
+	// Remove broken platforms
+	var i = platforms.length;
+	while (i--) {
+		var platform = platforms[i];
+		if (platform.broken) {
+			platforms.splice(i, 1);
+		}
+	}
+}
+
 function drawBackground(context){
 	context.drawImage(background.image, background.x, background.y);
 	context.drawImage(background.image, background.x, background.y - 1600);
 }
 
-function drawPlatform(context) {
-	context.drawImage(platform.image, platform.x, platform.y);
+function drawPlatforms(context) {
 	platforms.forEach(function(item, index, array) {
-		context.drawImage(platform.image, item.x, item.y);
+		context.drawImage(platformImg, item.x, item.y);
 	})
 	
 	
@@ -187,12 +219,6 @@ function setupGame() {
 	platformImg = new Image();
 	platformImg.src = "Platform.png";
 	
-	platform.image = platformImg;
-	platform.x = 0;
-	platform.y = 600;
-	platform.width = 64;
-	platform.height = 32;
-	
 	doodle.leftFace = doodleImgLeft;
 	doodle.rightFace = doodleImgRight;
 	doodle.image = doodleImgLeft;
@@ -205,7 +231,9 @@ function setupGame() {
 	doodle.width = 64;
 	doodle.height = 64;
 	
-	createPlatformChunk();
+	chunk_library.forEach( function (item, index, array) {
+		createPlatformChunk(item, -SCREEN_HEIGHT * index, index);
+	})
 	
 	setTimeout(main, 100, 0);
 }
@@ -213,22 +241,18 @@ function setupGame() {
 function update() {
 	updateDoodle();
 	updateBackground();
+	updatePlatforms();
 }
 
 function draw(context) {
 	drawBackground(context);
-	drawPlatform(context);
+	drawPlatforms(context);
 	drawDoodle(context);
-
 }
 
 function main(yCoord) {
 	const canvas = document.querySelector("#myCanvas");
 	var context = canvas.getContext('2d');
-	
-	context.clearRect(0,0,1000,1000);
-	context.fillStyle = 'green';
-	//drawARectangle(context, 350, yCoord);
 	
 	update();
 	draw(context);
